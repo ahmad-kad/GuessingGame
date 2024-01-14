cdeaf95185f7b6e31f8890d1b744c27a74b3eccc
Ahmad Kaddoura <kaddoura.ahmad@gmail.com>
2024-01-14 15:06:42 -0800

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by ahmad kaddoura on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
